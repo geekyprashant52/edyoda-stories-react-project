ee2e92eb3d3b3f03a0a4563a01881db62abee2c8
geekyprashant52 <61961514+geekyprashant52@users.noreply.github.com>
2021-07-29 20:54:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import CategoriesPage from "./Components/Categories Page/CategoriesPage";
+import HomePage from "./Components/Homepage/HomePage";
+import TopBar from "./Components/TopBar/TopBar";
 
 function App() {
+  const [slugName, setSlugName] = useState("");
+  const [position, setPosition] = useState(0);
+  const handleClick = (slug, pos) => {
+    setSlugName(slug);
+    setPosition(pos);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopBar />
+      <CategoriesPage handleClick={handleClick} position={position} />
+      <HomePage slug={slugName} />
     </div>
   );
 }
